1ac2f66238dbc7cca402b68b3e1955f952cca35e
Shri-456 <30828674+Shri-456@users.noreply.github.com>
2018-01-19 15:54:47 +0530

    Create 16.c

---
+++ 16.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.j>
+void main()
+{
+int i,n;
+for(i=0;i<=10;i++)
+{
+for(i=1;i<=n/2;i++)
+{
+if(n/2==0)
+{
+flag=1;
+break;
+}
+if(flag=0)
+{
+printf("it is a prime uum");
+}
+else
+{
+printf("/n it is not a prime num");
+}
+}
+}
+}
