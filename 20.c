ca20adb0bebd716a6dddfdbadee8427db0e57a47
Shri-456 <30828674+Shri-456@users.noreply.github.com>
2018-01-19 16:39:22 +0530

    Create 20.c

---
+++ 20.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,m,i,j,t;
+printf("enter a limit");
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+for(j=1;j<n;j++)
+{
+t=i*j;
+}
+}
+printf("%d=%d*%d",t,i,j);
+}
+
