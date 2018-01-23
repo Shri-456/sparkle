548712f24aea6c0db18fd49261fa3bc26b8f8076
Shri-456 <30828674+Shri-456@users.noreply.github.com>
2018-01-23 16:56:02 +0530

    Update and rename 26.c to .c

--- .c
+++ .c
@@ -1,13 +1,16 @@
 #include<stdio.h>
-void quicksort(int number[25],int first,int last){
+void quicksort(int number[25],int first,int last)
+{
    int i, j, pivot, temp;
 
-   if(first<last){
+   if(first<last)
+   {
       pivot=first;
       i=first;
       j=last;
 
-      while(i<j){
+      while(i<j)
+      {
          while(number[i]<=number[pivot]&&i<last)
             i++;
          while(number[j]>number[pivot])
