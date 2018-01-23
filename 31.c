af2db1bc064ffef6f89e5bf007a5d6681ff6b84d
Shri-456 <30828674+Shri-456@users.noreply.github.com>
2018-01-23 16:44:08 +0530

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int t=0,i=0;
+char s[20];
+printf("enter a string ");
+scanf("%s",s);
+for(i=0;i<=s[20];i++)
+{
+while(s[i]!=0)
+{
+t=t++;
+}
+printf("total words are %d",t);
+}
+}
