e2d93f567a0a93094f859067d6ca4a7e76fb8c57
Shri-456 <30828674+Shri-456@users.noreply.github.com>
2018-03-01 15:45:48 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,m,p;
+printf("enter two numbers");
+scanf("%d%d",&n,&m;
+p=n*m;
+if(p%2==0)
+{
+printf("\n it is even");
+}
+else
+{
+printf("\n it is odd");
+}
+}
